f8c747c48b1443c889da1ae892420b43d0d273e4
MustHardzik <banishedsanguine@wp.pl>
2023-11-09 00:44:03 +0100

    mh-addFuel propert added + minor changes

--- car/Musthardzik/index.ts
+++ car/Musthardzik/index.ts
@@ -1,48 +1,57 @@
+const MAX_FUEL_TANK_CAPACITY = 60
 class Car {
 
-	constructor(
-  private brand: string,
-  private model: string,
-  private version: string,
-  private year: number,
-  private isRunning: boolean,
-  private fuelLevel: number
+  constructor(
+    private brand: string,
+    private model: string,
+    private version: string,
+    private year: number,
+    private isRunning: boolean,
+    private fuelLevel: number
   ) {}
 
   startCar() {
-    if (this.fuelLevel > 1 && this.isRunning === false) {
+    if (this.fuelLevel >= 1 && !this.isRunning) {
       this.isRunning = true;
       alert("Engine has been turned on.")
     } else if (this.fuelLevel === 0) {
       alert("Fuel tank is empty. Check fuel level.")
-   } else if (this.isRunning === true) {
+    } else if (this.isRunning) {
       alert("Engine is already on.")
-   } else {
+    } else {
       alert("Something is wrong. Engine can't be turned on. Check fuel and fluids status, or visit local mechanic.")
     }
   }
-	
+
   stopCar() {
-    if(this.isRunning === true) {
+    if (this.isRunning) {
       this.isRunning = false;
       alert("Engine has been turned off.")
     } else {
       alert("Engine is already off.")
     }
   }
-  
-  fuelStatus() {
-  
+
+  addFuel() {
+    if (this.fuelLevel === MAX_FUEL_TANK_CAPACITY) {
+      alert("Fuel tank is fully filled")
+    } else if (this.fuelLevel < MAX_FUEL_TANK_CAPACITY && !this.isRunning) {
+      this.fuelLevel += 10
+      alert("Refueling in progress. Please wait. \nFuel in tank: " + this.fuelLevel)
+    } else {
+      alert("Engine is working. Turn off to proceed.")
+    }
+
   }
-  
+
   getProperties() {
     alert("Brand: " + this.brand + "\nModel: " + this.model + "\nVersion: " + this.version + "\nYear: " + this.year + "\nTurned On: " + this.isRunning + "\nFuel Status: " + this.fuelLevel)
   }
 }
-let sportCar = new Car("Alfa Romeo", "147", "1.6 T.S Distinctive", 2007, false, 60)
+let sportCar = new Car("Alfa Romeo", "147", "1.6 T.S Distinctive", 2007, false, 1)
 let propert = false
 while (!propert) {
-let input = prompt("Click to proceed!\n 1.Start engine\n 2.Stop engine\n 3.Check fuel level\n 4.Check car properties")
+  let input = prompt("Click to proceed!\n 1.Start engine\n 2.Stop engine\n 3.Pour fuel into tank\n 4.Check car properties")
   switch (input) {
     case "1":
       sportCar.startCar();
@@ -51,7 +60,7 @@ let input = prompt("Click to proceed!\n 1.Start engine\n 2.Stop engine\n 3.Check
       sportCar.stopCar();
       break;
     case "3":
-      sportCar.fuelStatus();
+      sportCar.addFuel();
       break;
     case "4":
       sportCar.getProperties();
