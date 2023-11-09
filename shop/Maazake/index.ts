0629fc1b64cb5a4b63382126c4505e5afe12bede
Mazak <adam951231535@gmail.com>
2023-11-09 18:25:45 +0100

    ma-shop-added: removeQuantityFromStock

--- shop/Maazake/index.ts
+++ shop/Maazake/index.ts
@@ -30,12 +30,22 @@ class Shop {
 	}
 	addQuantityToStock(name: string, quantity: number) {
 		let product = this.stock.find(product => product.name === name)
-
 		if (product) {
 			product.quantity += quantity
-			console.log(`Pomyślnie dodano ${name} x ${quantity} do magazynu sklepu`)
+			console.log(`Pomyślnie dodano ${name} x ${quantity} do magazynu sklepu.`)
 		} else {
-			console.log(`${name} nie ma nawet w magazynie sklepu`)
+			console.log(`W magazynie sklepu nie ma ${name}.`)
+		}
+	}
+	removeQuantityFromStock(name: string, quantity: number) {
+		let product: any = this.stock.find(product => product.name === name)
+		if (!product) {
+			console.log(`W magazynie sklepu nie ma ${name}.`)
+		} else if (product.quantity < quantity) {
+			console.log(`W magazynie sklepu nie ma aż tyle ${name}. Dostępne jest tylko ${product.quantity} sztuk.`)
+		} else {
+			product.quantity -= quantity
+			console.log(`Pomyślnie usunięto ${name} x ${quantity} z magazynu sklepu.`)
 		}
 	}
 	createShoppingCart(): ShoppingCart {
@@ -44,7 +54,7 @@ class Shop {
 	addProductToCart(shoppingCart: ShoppingCart, name: string, quantity: number) {
 		let product = this.stock.find(product => product.name === name)
 		if (!product || product.quantity === 0) {
-			console.log(`${name} nie ma w sklepie`)
+			console.log(`${name} nie ma w sklepie.`)
 		} else {
 			if (product.quantity >= quantity) {
 				let copyOfProduct = { ...product }
@@ -52,7 +62,7 @@ class Shop {
 				shoppingCart.addProduct(copyOfProduct)
 				product.quantity -= quantity
 			} else {
-				console.log(`Tego produktu jest tylko ${product.quantity} sztuk`)
+				console.log(`Tego produktu jest tylko ${product.quantity} sztuk.`)
 			}
 		}
 	}
@@ -64,7 +74,7 @@ class Shop {
 			if (product.quantity >= quantity) {
 				product.quantity -= quantity
 				shoppingCart.totalPrice -= product.price * quantity
-				console.log(`Pomyślnie usunięto ${name} x ${quantity} z koszyka`)
+				console.log(`Pomyślnie usunięto ${name} x ${quantity} z koszyka.`)
 			}
 		}
 	}
@@ -82,6 +92,8 @@ walmart.addProductToStock(new Product('chleb', 2, 5))
 walmart.addProductToStock(new Product('energy drink', 6, 3))
 walmart.addProductToStock(new Product('jabłko', 1, 10))
 walmart.addQuantityToStock('jabłko', 30)
+walmart.addQuantityToStock('czekolada', 2)
+walmart.removeQuantityFromStock('jabłko', 60)
 
 let cart = walmart.createShoppingCart()
 walmart.addProductToCart(cart, 'jabłko', 20)
