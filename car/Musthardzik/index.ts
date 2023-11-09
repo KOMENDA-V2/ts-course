6d25d287799483c30caba8e0109335d8b1af7de3
MustHardzik <banishedsanguine@wp.pl>
2023-11-09 14:49:29 +0100

    mh-driveCar propert added + final minor changes

--- car/Musthardzik/index.ts
+++ car/Musthardzik/index.ts
@@ -2,54 +2,79 @@ const MAX_FUEL_TANK_CAPACITY = 60
 class Car {
 
   constructor(
-    private brand: string,
-    private model: string,
-    private version: string,
-    private year: number,
-    private isRunning: boolean,
-    private fuelLevel: number
+    public brand: string,
+    public model: string,
+    public version: string,
+    public year: number,
+    public isRunning: boolean,
+    public fuelLevel: number,
+    public combustion: number,
   ) {}
 
   startCar() {
     if (this.fuelLevel >= 1 && !this.isRunning) {
       this.isRunning = true;
-      alert("Engine has been turned on.")
+      console.log("Engine has been turned on.")
     } else if (this.fuelLevel === 0) {
-      alert("Fuel tank is empty. Check fuel level.")
+      console.log("Fuel tank is empty. Check fuel level.")
     } else if (this.isRunning) {
-      alert("Engine is already on.")
+      console.log("Engine is already on.")
     } else {
-      alert("Something is wrong. Engine can't be turned on. Check fuel and fluids status, or visit local mechanic.")
+      console.log("Something is wrong. Engine can't be turned on. Check fuel and fluids status, or visit local mechani			c.")
     }
   }
 
   stopCar() {
     if (this.isRunning) {
       this.isRunning = false;
-      alert("Engine has been turned off.")
+      console.log("Engine has been turned off.")
     } else {
-      alert("Engine is already off.")
+      console.log("Engine is already off.")
     }
   }
 
-  addFuel() {
+  addFuel(fuel: number) {
     if (this.fuelLevel === MAX_FUEL_TANK_CAPACITY) {
-      alert("Fuel tank is fully filled")
+      console.log("Fuel tank is fully filled")
     } else if (this.fuelLevel < MAX_FUEL_TANK_CAPACITY && !this.isRunning) {
-      this.fuelLevel += 10
-      alert("Refueling in progress. Please wait. \nFuel in tank: " + this.fuelLevel)
+      this.fuelLevel += fuel
+      console.log("Refueling in progress. Please wait. \nFuel in tank: " + this.fuelLevel)
     } else {
-      alert("Engine is working. Turn off to proceed.")
+      console.log("Engine is working. Turn off to proceed.")
     }
 
   }
 
   getProperties() {
-    alert("Brand: " + this.brand + "\nModel: " + this.model + "\nVersion: " + this.version + "\nYear: " + this.year + "\nTurned On: " + this.isRunning + "\nFuel Status: " + this.fuelLevel)
+    console.log("Brand: " + this.brand + "\nModel: " + this.model + "\nVersion: " + this.version + "\nYear: " +
+      this.year + "\nTurned On: " + this.isRunning + "\nFuel Status: " + this.fuelLevel + "\nCombustion Status: " +
+      this.combustion + " l/100km")
   }
 }
-let sportCar = new Car("Alfa Romeo", "147", "1.6 T.S Distinctive", 2007, false, 1)
-let propert = false
+
+let sportCar = new Car("Alfa Romeo", "147", "1.6 T.S Distinctive", 2007, false, 12, 12)
+
+
+
+function driveCar(distance: number, car: Car) {
+  if (car.isRunning && car.fuelLevel >= 1) {
+    let fuelUsed = parseFloat((distance / car.combustion).toFixed(2));
+    if (car.fuelLevel >= fuelUsed) {
+      console.log(`${car.brand} ${car.model} has traveled ${distance} km, using ${fuelUsed} liters of fuel.`)
+      car.fuelLevel = car.fuelLevel - fuelUsed;
+    } else {
+      console.log(`${car.brand} ${car.model} has no fuel in tank. Visit local gas station to pump some.`)
+    }
+
+  } else {
+    console.log("Something is wrong. Engine can't be turned on. Check fuel and fluids status, or visit local mechanic.")
+  }
+}
+sportCar.addFuel(10);
+sportCar.startCar();
+driveCar(100, sportCar)
+
+/* let propert = false
 while (!propert) {
   let input = prompt("Click to proceed!\n 1.Start engine\n 2.Stop engine\n 3.Pour fuel into tank\n 4.Check car properties")
   switch (input) {
@@ -69,4 +94,4 @@ while (!propert) {
       propert = true;
       break;
   }
-}
+} */
