3e7fd7dbedd8cdb76c4c4ff590bf1785d517673c
MustHardzik <banishedsanguine@wp.pl>
2023-11-17 19:00:54 +0100

    mh-export board removed

--- dicegame/Musthardzik/index.ts
+++ dicegame/Musthardzik/index.ts
@@ -3,7 +3,7 @@ import { Board } from "./board";
 import { PlayerGenerator } from "./playerGenerator";
 
 let players = PlayerGenerator.generate();
-export const board = new Board(50, players, 100, 12);
+const board = new Board(50, players, 100, 12);
 try {
   while (true) {
     board.performTurn();
